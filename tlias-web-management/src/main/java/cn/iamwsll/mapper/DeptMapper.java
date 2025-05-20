5262c86ee06955793bcb7e7c391a3c13b3164291
微生麓黎 <1481121704@qq.com>
2025-05-20 22:40:52 +0800

    fix the bug of time

--- tlias-web-management/src/main/java/cn/iamwsll/mapper/DeptMapper.java
+++ tlias-web-management/src/main/java/cn/iamwsll/mapper/DeptMapper.java
@@ -2,6 +2,8 @@ package cn.iamwsll.mapper;
 
 import cn.iamwsll.pojo.Dept;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.Results;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
