c44a5542e95516c7d1feee5487e94bfcb42ac880
微生麓黎 <1481121704@qq.com>
2025-05-28 21:50:05 +0800

    完成基本的员工查询优化2

--- tlias-web-management/src/main/java/cn/iamwsll/service/impl/EmpServiceImpl.java
+++ tlias-web-management/src/main/java/cn/iamwsll/service/impl/EmpServiceImpl.java
@@ -9,6 +9,7 @@ import com.github.pagehelper.Page;
 import com.github.pagehelper.PageHelper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.util.List;
